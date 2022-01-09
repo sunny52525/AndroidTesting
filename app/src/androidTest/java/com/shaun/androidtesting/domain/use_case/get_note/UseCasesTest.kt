d70c9439a133109292a0dde203096c35407b469f
Sunny <30768018+sunny52525@users.noreply.github.com>
2022-01-09 12:22:48 +0530

    added more use case test

--- app/src/androidTest/java/com/shaun/androidtesting/domain/use_case/get_note/UseCasesTest.kt
+++ app/src/androidTest/java/com/shaun/androidtesting/domain/use_case/get_note/UseCasesTest.kt
@@ -6,11 +6,12 @@ import com.shaun.androidtesting.common.equal
 import com.shaun.androidtesting.data.local.dao.NoteDao
 import com.shaun.androidtesting.data.local.database.NoteDatabase
 import com.shaun.androidtesting.data.local.dto.NoteDto
+import com.shaun.androidtesting.data.local.dto.toNotes
 import com.shaun.androidtesting.domain.repository.NoteRepository
 import com.shaun.androidtesting.getOrAwaitValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -19,7 +20,7 @@ import javax.inject.Inject
 import javax.inject.Named
 
 @HiltAndroidTest
-class AddNoteUseCaseTest {
+class UseCasesTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
@@ -29,6 +30,7 @@ class AddNoteUseCaseTest {
     @Inject
     @Named("test_db")
     lateinit var database: NoteDatabase
+
     @Inject
     @Named("fake_dao")
     lateinit var noteDao: NoteDao
@@ -38,14 +40,18 @@ class AddNoteUseCaseTest {
     lateinit var repository: NoteRepository
     private lateinit var noteUseCase: AddNoteUseCase
     private lateinit var deleteNoteUseCase: DeleteNoteUseCase
-    private lateinit var getNoteUseTestCase:GetNoteUseCase
+    private lateinit var getNoteUseTestCase: GetNoteUseCase
+    private lateinit var editNoteUseCaseTest: EditNoteUseCase
+    private lateinit var getSingleNoteUseCase: GetSingleNoteUseCase
 
     @Before
     fun setup() {
         hiltRule.inject()
         noteUseCase = AddNoteUseCase(repository)
-        deleteNoteUseCase= DeleteNoteUseCase(repository)
-        getNoteUseTestCase= GetNoteUseCase(repository)
+        deleteNoteUseCase = DeleteNoteUseCase(repository)
+        getNoteUseTestCase = GetNoteUseCase(repository)
+        editNoteUseCaseTest = EditNoteUseCase(repository)
+        getSingleNoteUseCase = GetSingleNoteUseCase(repository)
     }
 
     @After
@@ -55,15 +61,15 @@ class AddNoteUseCaseTest {
 
 
     @Test
-    fun testUseCaseSuccessAdd()= runBlockingTest{
+    fun testUseCaseSuccessAdd() = runBlocking {
 
-        val note=NoteDto(
+        val note = NoteDto(
             title = "Note Use Case",
             body = "Note body",
             uid = 2
         )
         val addNote = noteUseCase(note)
-        val expectedResult=Resource.Success<String?>(null)
+        val expectedResult = Resource.Success<String?>(null)
 
         assert(addNote.equal(expectedResult))
         val allNotes = noteDao.getNotes().getOrAwaitValue()
@@ -73,8 +79,8 @@ class AddNoteUseCaseTest {
     }
 
     @Test
-    fun deleteNoteUseCase()= runBlockingTest {
-        val noteDto=NoteDto(
+    fun deleteNoteUseCase() = runBlocking {
+        val noteDto = NoteDto(
             title = "New note",
             body = "new body",
             uid = 10
@@ -84,7 +90,7 @@ class AddNoteUseCaseTest {
 
         deleteNoteUseCase(noteDto.uid)
 
-        val allNotes= noteDao.getNotes().getOrAwaitValue()
+        val allNotes = noteDao.getNotes().getOrAwaitValue()
 
         assert(allNotes.contains(noteDto).not())
 
@@ -92,25 +98,73 @@ class AddNoteUseCaseTest {
     }
 
 
-
     @Test
-    fun getNotesUseCaseTest()= runBlockingTest{
+    fun getNotesUseCaseTest() = runBlocking {
 
-         val notes = (1..10).map {
-            val note =NoteDto(
+        val notes = (1..10).map {
+            val note = NoteDto(
                 title = "note $it",
                 body = "note body $it",
                 uid = it.toLong()
-            ).also { note->
+            ).also { note ->
                 noteUseCase(note)
             }
-             note
+            note
         }
         val allNotes = noteDao.getNotes().getOrAwaitValue()
 
         assert(
             notes.containsAll(allNotes)
         )
+
+
+    }
+
+
+    @Test
+    fun editUseCaseTest() = runBlocking {
+        val noteDto = NoteDto(
+            title = "New note",
+            body = "new body",
+            uid = 40
+        )
+
+        noteDao.insertNote(noteDto)
+
+        val newNote = noteDto.copy(
+            title = "Edited Note",
+            body = "Edited Body"
+        )
+
+
+        editNoteUseCaseTest(
+            noteId = noteDto.uid, title = newNote.title, body = newNote.body
+        )
+
+        val newNoteFromDB = noteDao.getNotes().getOrAwaitValue().firstOrNull {
+            noteDto.uid == it.uid
+        }
+
+        assert(newNoteFromDB != null)
+        assert(newNoteFromDB == newNote)
+        assert(newNoteFromDB != noteDto)
+
+    }
+
+    @Test
+    fun getSingleNoteUse() = runBlocking {
+        val noteDto = NoteDto(
+            title = "New note",
+            body = "new body",
+            uid = 40
+        )
+
+        noteDao.insertNote(noteDto)
+
+        val noteFromDB = getSingleNoteUseCase(noteDto.uid)
+
+        assert(noteFromDB is Resource.Success)
+        assert(noteFromDB.data == noteDto.toNotes())
 
 
     }
